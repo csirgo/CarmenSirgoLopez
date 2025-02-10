eb4f323739764cf49260deb007bee2c6136f9417
Carmen Sirgo Lopez <csirgo@hiberus.com>
2025-02-10 13:36:46 +0100

    added test endpoint

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-const express = require('express');
-const path = require('path');
-const i18next = require('i18next');
-const i18nextMiddleware = require('i18next-express-middleware');
-
+const express = require("express");
 const app = express();
 
-// Sirve los archivos locales de forma estática
-app.use('/locales', express.static(path.join(__dirname, 'locales')));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Configura i18next (si es necesario)
-i18next.use(i18nextMiddleware.LanguageDetector).init({
-  fallbackLng: 'en',
-  resources: {
-    en: { translation: require('./locales/en.json') },
-    es: { translation: require('./locales/es.json') },
-  },
-});
+app.get("/hello", (req, res) => res.send("Hello World"));
 
-// Usa middleware de i18next
-app.use(i18nextMiddleware.handle(i18next));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// Rutas de Express
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// Configura puerto
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+module.exports = app;
